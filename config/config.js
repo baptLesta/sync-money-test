1e77224c1101391075fcce4008c4ad5aad35c233
lestavelbaptiste <baptiste.lestavel@hei.fr>
2019-01-31 10:55:53 +0100

    Removed test for deploy. Everything works

--- config/config.js
+++ config/config.js
@@ -9,7 +9,21 @@ const envVarsSchema = Joi.object({
     .allow(['development', 'production', 'test', 'provision'])
     .default('development'),
   PORT: Joi.number()
-    .default(4040)
+    .default(4040),
+  MONGOOSE_DEBUG: Joi.boolean()
+    .when('NODE_ENV', {
+      is: Joi.string().equal('development'),
+      then: Joi.boolean().default(true),
+      otherwise: Joi.boolean().default(false)
+    }),
+  JWT_EXPIRATION: Joi,
+  JWT_ENCRYPTION: Joi,
+  JWT_SECRET: Joi.string().required()
+    .description('JWT Secret required to sign'),
+  MONGO_HOST: Joi.string().required()
+    .description('Mongo DB host url'),
+  MONGO_PORT: Joi.number()
+    .default(27017)
 }).unknown()
   .required();
 
@@ -20,7 +34,15 @@ if (error) {
 
 const config = {
   env: envVars.NODE_ENV,
-  port: envVars.PORT
+  port: envVars.PORT,
+  mongooseDebug: envVars.MONGOOSE_DEBUG,
+  jwtSecret: envVars.JWT_SECRET,
+  jwtEncryption: envVars.JWT_ENCRYPTION,
+  jwtExpiration: envVars.JWT_EXPIRATION,
+  mongo: {
+    host: envVars.MONGO_HOST,
+    port: envVars.MONGO_PORT
+  }
 };
 
 module.exports = config;
